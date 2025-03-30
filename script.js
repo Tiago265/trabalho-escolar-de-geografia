e8e7428eb95f4bdd7d2b95dc6882947d5d5a121f
Tiago265 <futlastf998@gmail.com>
2025-03-30 17:48:42 -0300

    trabalho de geografia

---
+++ script.js
@@ -0,0 +1,8 @@
+function depoimento(){
+    let nota = document.getElementById('nota').value;1
+    if(nota >= 7){
+        alert("Muito Obrigado pela nota.")
+    }else{
+        alert("Na próxima eu melhoro!")
+    }
+}
